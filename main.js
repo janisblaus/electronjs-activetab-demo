883197fd32facf35fd2ddade9973f4288dff2a22
Jānis Blaus <janis@netcore.lv>
2019-07-30 16:07:12 +0300

    mac support

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const {app, BrowserWindow, ipcMain} = require('electron')
 const { exec, execFileSync, execFile } = require('child_process');
+const applescript = require('applescript');
 
 var mainWindow;
 
@@ -27,19 +28,35 @@ function initialise(){
 
   mainWindow.webContents.once('dom-ready', () => {
       
-      setInterval(function(){
-        execFile('./bin/win/activetab', (err, stdout, stderr) => {
-          if (err) {
-            console.error(err);
-            return;
-          }
 
-          //send to renderer
-          if( mainWindow ){
-            mainWindow.webContents.send('store-data', stdout);
+      if( process.platform == 'win32' ){
+        setInterval(function(){
+          execFile('./bin/win/activetab', (err, stdout, stderr) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+
+            //send to renderer
+            if( mainWindow ){
+              mainWindow.webContents.send('store-data', stdout);
+            }
+          });
+        },2000);
+      }
+
+
+      if( process.platform == 'darwin' ){
+        const script = 'tell application "Google Chrome" to return URL of active tab of front window';
+
+        applescript.execString(script, (err, rtn) => {
+          if (err) {
+            // Something went wrong!
           }
+          
+          mainWindow.webContents.send('store-data', rtn);
         });
-      },2000);
+      }
   })
 
   // Emitted when the window is closed.
